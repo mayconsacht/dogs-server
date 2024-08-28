d45894beeed26d43cb1c25792d6e7001b9d17c65
mayconsacht <maycon.sacht@gmail.com>
2024-08-28 04:47:05 -0300

    feature: add cors config

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express, { Application } from 'express';
 import userRoutes from './routes/photoRoutes';
-import * as dotenv from 'dotenv';
+import cors from 'cors';
 
 const app: Application = express();
 
-app.use(express.json());
+app.use(
+  cors({
+    origin: 'https://dogs.mayconsacht.com',
+    methods: 'POST, GET, PUT, DELETE',
+    allowedHeaders: 'Authorization, Content-Type',
+  }),
+  express.json()
+);
 
 app.use('/api', userRoutes);
 
