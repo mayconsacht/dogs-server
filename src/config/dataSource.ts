e8141a7216567477161a80b35f743c89ccab4df5
mayconsacht <maycon.sacht@gmail.com>
2024-08-28 00:02:25 -0300

    fix: connection ssl

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -13,6 +13,9 @@ export const AppDataSource = new DataSource({
   database: process.env.POSTGRES_DB,
   synchronize: false,
   logging: false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   entities: ['src/entity/**/*.ts'],
   migrations: ['src/migration/**/*.ts'],
   subscribers: ['src/subscriber/**/*.ts'],
