cc0c4fe2b6b80b65870f8a10afbfcbaeb936b31d
mayconsacht <maycon.sacht@gmail.com>
2024-09-02 17:25:57 -0300

    log: user

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,6 +5,7 @@ import bcrypt from 'bcryptjs';
 export const getUser = async (req: Request, res: Response) => {
   const user = userService.findUserBy({ id: req.user?.id });
   if (user) {
+    console.log('user ' + JSON.stringify(user));
     res.status(200).json(user);
   } else {
     res.status(404).json({ message: 'User not found' });
